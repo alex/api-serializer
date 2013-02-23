7a5f4ea32103f58aa23650f48889118ee9c0260c
Alex Gaynor <alex.gaynor@rd.io>
2013-02-23 10:46:06 -0800

    another test

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,14 +1,19 @@
 from api_serialization import SerializationAdapter, SerializationContext
 
 
+class Artist(object):
+    serialization_adapter = SerializationAdapter()
+
+    @serialization_adapter.converter("name")
+    def convert_name(self, ctx):
+        ctx.write("ABBA")
+
+
 class TestConversion(object):
     def test_simple_convert(self):
-        class Artist(object):
-            serialization_adapter = SerializationAdapter()
-
-            @serialization_adapter.converter("name")
-            def convert_name(self, ctx):
-                ctx.write("ABBA")
-
         ctx = SerializationContext(fields=["name"])
         assert ctx.write(Artist()) == '{"name":"ABBA"}'
+
+    def test_no_fields(self):
+        ctx = SerializationContext(fields=[])
+        assert ctx.write(Artist()) == "{}"
